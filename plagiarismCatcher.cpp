8aa7134fe4404ed4b03b09b34f0f088f763d08ac
Jin Lee <jinolemo@gmail.com>
2019-12-03 14:03:14 -0600

    Milestone I
    - Added clean()
    - Still segfaults when file is empty (e.g. lah10.txt)

--- plagiarismCatcher.cpp
+++ plagiarismCatcher.cpp
@@ -12,43 +12,48 @@
 using namespace std;
 
 int getdir(string dir, vector<string> &files);
+void clean(string &word);
 
 int main() {
    string dir = string("sm_doc_set");
    vector<string> files = vector<string>();
    int n = 6; // n-word sequences   
-
    getdir(dir, files);
    
    //ifstream myfile; 
    for (int i = 2; i < files.size(); i++) {
       string fileptr = dir + "/" + files[i];
        cout << endl << fileptr << endl;
-      
       ifstream myfile;
       myfile.open(fileptr.c_str());
+      
       if (myfile.is_open()) {
          string word;
 	 vector<string> sequence; 
-
-         while (!myfile.eof()) {// extract words from file
+      
+         while (myfile) {// extract words from file
             while (sequence.size() < n && !myfile.eof()) {
                myfile >> word;
-               if (word.length() != 0) {
+               clean(word);
+
+               if (word.length() > 0) {
                   sequence.push_back(word);
                }
             }
-    
             string nChunk;
             for (int j = 0; j < sequence.size(); j++) {
                nChunk = nChunk + sequence[j];
             }
             cout << nChunk << endl;
+    
             sequence.erase(sequence.begin());
-         }
+         } 
       }
+
       else cout << "Unable to open file" << endl;
+
       myfile.close();
+cout << "DEBUG : iteration " << i << endl;
    }
    return 0;
 }
@@ -72,6 +77,21 @@ int getdir(string dir, vector<string> &files) {
    return 0;
 }
 
+/* ---clean---
+   PRE: Takes address of string to be cleaned
+   POST: Removes punctuation and special characters, makes all char lowercase
+*/
+void clean(string &word) {
+   for (int i = 0; i < word.length(); i++) {// check if each char is a lowercase alphabet
+      if ('A' <= word[i] && word[i] <= 'Z') {// make lowercase
+         word[i] = word[i] + 32;
+      }
+      else if (word[i] < 'a' || word[i] > 'z') {// remove special characters
+         word.erase(word.begin() + i);
+         i--;
+      }
+   }
+}
 /* --- plagiarismCatcher---
    PRE: Takes command line parameters for the path from
    the executable program to the text files and n (length of word sequence)
